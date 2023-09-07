97cb1d87bbfa9e08c56613b0942ab312d36b8811
Abhay Maurya <127673236+AbhayGRT@users.noreply.github.com>
2023-09-07 21:26:27 +0530

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-# Update the package list and install PostgreSQL
-sudo apt update
-sudo apt install postgresql postgresql-contrib
+# Update the package list
+sudo apt-get update
+
+# Install PostgreSQL
+sudo apt-get install -y postgresql
 
 # Start and enable the PostgreSQL service
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
-# Set a password for the default PostgreSQL user (postgres)
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'your_password';"
+# Verify the installation
+sudo -u postgres psql -c "SELECT version();"
 
-# Create a new PostgreSQL user and database (optional)
-# Replace 'new_user' and 'new_db' with your desired username and database name
-# sudo -u postgres psql -c "CREATE USER new_user WITH PASSWORD 'new_password';"
-# sudo -u postgres psql -c "CREATE DATABASE new_db;"
-# sudo -u postgres psql -c "ALTER ROLE new_user SET client_encoding TO 'utf8';"
-# sudo -u postgres psql -c "ALTER ROLE new_user SET default_transaction_isolation TO 'read committed';"
-# sudo -u postgres psql -c "ALTER ROLE new_user SET timezone TO 'UTC';"
-# sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE new_db TO new_user;"
+# Optional: Set a password for the PostgreSQL user "postgres"
+sudo -u postgres psql <<EOF
+\password postgres
+EOF
 
-# Optionally, configure PostgreSQL to allow remote connections (modify pg_hba.conf and postgresql.conf)
-# sudo nano /etc/postgresql/{version}/main/pg_hba.conf
-# sudo nano /etc/postgresql/{version}/main/postgresql.conf
-# Replace {version} with your PostgreSQL version (e.g., 13)
+# Optional: Create a new database (replace "yourdb" with your desired database name)
+sudo -u postgres psql <<EOF
+CREATE DATABASE yourdb;
+EOF
 
-# Restart PostgreSQL to apply any configuration changes
-# sudo systemctl restart postgresql
-
-echo "PostgreSQL installation complete!"
-
+echo "PostgreSQL installation completed."
