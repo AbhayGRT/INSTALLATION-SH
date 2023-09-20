b0cb4a3d230e1af90dcfd377ee9d292e728d839e
Abhay Maurya <127673236+AbhayGRT@users.noreply.github.com>
2023-09-20 20:01:43 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -16,3 +16,7 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
+
+
+# sudo apt install docker.io
+# sudo chown $USER /var/run/docker.sock
